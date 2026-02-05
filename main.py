f5457a1246bb02b4edebf0f3e695f1b62a4f724d
ivan-bohash <iv.bohash@gmail.com>
2026-02-05 18:14:07 +0100

    fix docker compose and main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import asyncio
 import warnings
 
+from redis import Redis
+from rq import Queue
+
 from src.database.db import init_db
 from src.scheduler.run_scheduler import init_schedule
-from src.scheduler.tasks import execute_scraping
+from src.scheduler.tasks import run_autoria_scraper
 
 
 # ignore future warnings in logs
 warnings.simplefilter("ignore", category=FutureWarning)
+
+# first time run
+redis_conn = Redis(host='redis', port=6379)
+q = Queue('default', connection=redis_conn)
 
 async def main():
     """
@@ -23,8 +30,8 @@ async def main():
     except Exception as e:
         print(f"Scheduler error: {e}")
 
-    # to run scraping on first run
-    await execute_scraping()
+    q.enqueue(run_autoria_scraper)
+
 
 
 if __name__ == "__main__":
